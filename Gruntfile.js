edeb62dd641b42090f354048f2423516adb9ecfc
Brian Lalor <blalor@bravo5.org>
2013-10-02 15:29:45 -0400

    Run tests outside of Grunt
    Spawn a process.  Grunt doesn't cooperate when trying to run tasks
    from inside of another task (which is what the grunt-mocha-test plugin
    does).

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,45 +16,33 @@ module.exports = function(grunt) {
             all: [
                 "Gruntfile.js",
                 "tasks/*.js",
-                "<%= mochaTest.test.src %>",
+                "test/**/*.js",
             ],
             options: {
                 jshintrc: ".jshintrc",
             },
         },
-
-        // Before generating any new files, remove any previously-created files.
-        clean: {
-            tests: ["tmp"],
-        },
-
-        // Configuration to be run (and then tested).
-        prepare_install: {
-            options: {
-                installPrefix: "/usr/local"
-            }
-        },
-
-        // Unit tests
-        mochaTest: {
-            test: {
-                src: [ "test/**/*.js" ]
-            }
-        }
     });
-
-    // Actually load this plugin"s task(s).
-    grunt.loadTasks("tasks");
 
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.loadNpmTasks("grunt-mocha-test");
 
-    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-    // plugin"s task(s), then test the result.
-    grunt.registerTask("test", ["clean", "prepare_install", "mochaTest"]);
 
+    grunt.registerTask("mochaTest", "runs mocha in a child process", function() {
+        var done = this.async();
+        
+        // spawn a child process to run mocha, and send the output to our own
+        // stdio.
+        grunt.util.spawn({
+            cmd: "node_modules/.bin/mocha",
+            opts: {
+                stdio: [ null, process.stdout, process.stderr ]
+            }
+        }, function(err /* , result, code */) {
+            done(err);
+        });
+    });
+    
     // By default, lint and run all tests.
-    grunt.registerTask("default", ["jshint", "test"]);
+    grunt.registerTask("test", ["jshint", "mochaTest"]);
 };
--- test/prepare_install_test.js
+++ test/prepare_install_test.js
@@ -3,47 +3,134 @@
 
 var expect = require("chai").expect;
 var assert = require("chai").assert;
-var grunt  = require("grunt");
 var _      = require("lodash");
 var fs     = require("fs");
 var path   = require("path");
 
+var temp = require("temp");
+temp.track();
+
+// http://stackoverflow.com/a/14801711/53051
+/**
+ * Removes a module from the cache
+ */
+require.uncache = function (moduleName) {
+    // Run over the cache looking for the files
+    // loaded by the specified module name
+    require.searchCache(moduleName, function (mod) {
+        delete require.cache[mod.id];
+    });
+};
+
+/**
+ * Runs over the cache to search for all the cached
+ * files
+ */
+require.searchCache = function (moduleName, callback) {
+    // Resolve the module identified by the specified name
+    var mod = require.resolve(moduleName);
+
+    // Check if the module has been resolved and found within
+    // the cache
+    if (mod && ((mod = require.cache[mod]) !== undefined)) {
+        // Recursively go over the results
+        (function run(mod) {
+            // Go over each of the module's children and
+            // run over it
+            mod.children.forEach(function (child) {
+                run(child);
+            });
+
+            // Call the specified callback providing the
+            // found module
+            callback(mod);
+        })(mod);
+    }
+};
+
+
 // tests need to run after prepare_install task in Gruntfile.js for this
 // project.  Kludgy, but it'll work for now…
 describe("grunt-prepare-install", function() {
-    var pkgJson;
-    var gruntfile;
-    var installBase;
+    // wikked long timeout
+    this.timeout(120000);
     
-    before(function() {
+    // shared cache dir, to speed up downloads
+    var npmCacheDir = path.join(path.resolve(), "tmp/npm-cache");
+    
+    var pkgJson;
+    var installBase;
+    var grunt;
+    
+    beforeEach(function() {
+        grunt = require("grunt");
         pkgJson = grunt.file.readJSON("package.json");
+
+        // Actually load this plugin"s task(s).
+        grunt.loadTasks("tasks");
         
-        gruntfile = require("../Gruntfile")(grunt);
+        // Project configuration.
+        grunt.initConfig({
+            prepare_install: {
+                options: {
+                    tmpDir: temp.mkdirSync(),
+                    cacheDir: npmCacheDir,
+                    installPrefix: "/usr/local"
+                }
+            }
+        });
         
         // the root of the installed module
         installBase = path.join(
-            "tmp/package-root",
+            grunt.config.get("prepare_install.options.tmpDir"),
+            "package-root",
             grunt.config.get("prepare_install.options.installPrefix"),
             "lib/node_modules/grunt-prepare-install"
         );
     });
+    
+    afterEach(function() {
+        grunt = null;
+        require.uncache("grunt");
+        require.uncache("npm");
+    });
 
-    it("installs to configured path", function() {
-        assert(grunt.file.exists(installBase));
-        assert(grunt.file.exists(installBase, "node_modules"));
+    it("installs to configured path", function(done) {
+        grunt.task.run("prepare_install");
+        
+        grunt.task.options({
+            done: function() {
+                assert(grunt.file.exists(installBase));
+                assert(grunt.file.exists(installBase, "node_modules"));
+                
+                done();
+            }
+        });
+        
+        grunt.task.start();
     });
     
-    it("only installs runtime dependencies", function() {
-        // find all installed modules by reading all entries in the node_modules
-        // directory and removing ., .., and .bin.
-        var installedDeps = _.difference(
-            fs.readdirSync(path.join(installBase, "node_modules")),
-            [ ".", "..", ".bin" ]
-        );
+    it("only installs runtime dependencies", function(done) {
+        grunt.task.run("prepare_install");
         
-        expect(_.difference(
-            Object.keys(pkgJson.dependencies),
-            installedDeps
-        )).to.be.empty;
+        grunt.task.options({
+            done: function() {
+                // find all installed modules by reading all entries in the node_modules
+                // directory and removing ., .., and .bin.
+                var installedDeps = _.difference(
+                    fs.readdirSync(path.join(installBase, "node_modules")),
+                    [ ".", "..", ".bin" ]
+                );
+                
+                expect(_.difference(
+                    Object.keys(pkgJson.dependencies),
+                    installedDeps
+                )).to.be.empty;
+                
+                done();
+            }
+        });
+        
+        grunt.task.start();
     });
 });
