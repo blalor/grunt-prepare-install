dbfc6009d3061f01084707ae870c6def513d896c
Brian Lalor <blalor@bravo5.org>
2013-10-03 21:10:43 -0400

    Clean up packageRoot, exclude working directories
    If the cacheDir and packageRoot directories aren't filtered out,
    they'll appear in the installed project, making it very large and
    slowing stuff down a LOT.

--- tasks/prepare_install.js
+++ tasks/prepare_install.js
@@ -8,11 +8,42 @@
 
 "use strict";
 
-var path = require("path");
-var Q    = require("q");
+var path   = require("path");
+var Q      = require("q");
+var rimraf = require("rimraf");
 
 module.exports = function(grunt) {
 
+    // stolen almost verbatim from grunt-contrib-clean
+    function clean(filepath) {
+        if (! grunt.file.exists(filepath)) {
+            return false;
+        }
+
+        grunt.log.write("Cleaning " + filepath + " … ");
+
+        if (grunt.file.isPathCwd(filepath)) {
+            grunt.verbose.error();
+            grunt.fail.warn("Cannot delete the current working directory.");
+            
+            return false;
+        } else if (! grunt.file.isPathInCwd(filepath)) {
+            grunt.verbose.error();
+            grunt.fail.warn("Cannot delete files outside the current working directory.");
+            
+            return false;
+        }
+
+        try {
+            rimraf.sync(filepath);
+            
+            grunt.log.ok();
+        } catch (e) {
+            grunt.log.error();
+            grunt.fail.warn("Unable to delete '" + filepath + "' file (" + e.message + ").", e);
+        }
+    }
+    
     // run "npm install $PWD".  This will be our install package, with full
     // binary modules.
     grunt.registerTask("prepare_install", "npm install", function() {
@@ -36,20 +67,53 @@ module.exports = function(grunt) {
             opts.packageRoot = path.join(opts.tmpDir, "package-root");
         }
         
-        var done = this.async();
+        // https://npmjs.org/doc/misc/npm-config.html#ignore
+        var ignorePaths = [
+            "node_modules", // definitely don't want this
+        ];
         
-        // despite loglevel: "warn" we still get a summary of what was
-        // installed, plus gyp (?) output
-        Q.ninvoke(require("npm"), "load", {
+        // add the portions of the temporary directory paths that exist under
+        // the project root (if it exists) to ignorePaths
+        [ opts.cacheDir, opts.packageRoot ].forEach(function(d) {
+            if (d.indexOf(path.resolve()) === 0) {
+                ignorePaths.push(d.substr(path.resolve().length + 1));
+            }
+        });
+        
+        // https://npmjs.org/doc/misc/npm-config.html
+        var npmOpts = {
             production: true,
             "engine-strict": opts.npm.engineStrict,
             cache: opts.cacheDir,
             prefix: path.join(opts.packageRoot, opts.installPrefix),
+            ignore: ignorePaths.join(" "),
             global: true,
             loglevel: "warn"
-        }).then(function(npm) {
+        };
+        
+        clean(opts.packageRoot);
+        
+        var done = this.async();
+        
+        grunt.verbose.writeflags(npmOpts, "npm options");
+        
+        
+        // despite loglevel: "warn" we still get a summary of what was
+        // installed, plus gyp (?) output
+        Q.ninvoke(require("npm"), "load", npmOpts).then(function(npm) {
             // npm install $PWD
+            grunt.log.writeln("Installing module");
+            
             return Q.ninvoke(npm.commands, "install", [ path.resolve() ]);
-        }).done(done, done);
+        }).done(function() {
+            grunt.log.ok();
+
+            done();
+        }, function(err) {
+            grunt.log.error();
+            grunt.fail.warn("npm install failed: " + err.message, err);
+
+            done(err);
+        });
     });
 };
